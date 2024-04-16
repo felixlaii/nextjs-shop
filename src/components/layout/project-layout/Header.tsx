48e9d46e90ebf428c6025891e6dd1a886bcba22c
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-04-15 20:41:14 -0400

    import in header

--- src/components/layout/project-layout/Header.tsx
+++ src/components/layout/project-layout/Header.tsx
@@ -1,3 +1,8 @@
+import React, { Fragment, useEffect, useRef, useState } from "react";
+import { HeaderProps } from "@/types/component-types";
+import Link from "next/link";
+import clsx from "clsx";
+
 export const Header: React.FC = () => {
   return <div></div>;
 };
