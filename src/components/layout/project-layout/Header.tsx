18221c05cfdd58e6184e9e0072f77d8225b0095e
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-04-15 20:41:40 -0400

    created function for use clickoutside

--- src/components/layout/project-layout/Header.tsx
+++ src/components/layout/project-layout/Header.tsx
@@ -3,6 +3,28 @@ import { HeaderProps } from "@/types/component-types";
 import Link from "next/link";
 import clsx from "clsx";
 
+export function useOnClickOutside<T extends HTMLDivElement>(
+  ref: React.RefObject<T>,
+  handler: (e: any) => void
+) {
+  useEffect(() => {
+    const listener = (event: any) => {
+      if (!ref.current || ref.current.contains(event.target as Node)) {
+        return;
+      }
+      handler(event);
+    };
+
+    document.addEventListener("mousedown", listener);
+    document.addEventListener("touchstart", listener);
+
+    return () => {
+      document.removeEventListener("mousedown", listener);
+      document.removeEventListener("touchstart", listener);
+    };
+  }, [ref, handler]);
+}
+
 export const Header: React.FC = () => {
   return <div></div>;
 };
