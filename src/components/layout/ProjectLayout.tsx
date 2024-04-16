d06ed6734b2ca5c2b8e62c502731de904683c43c
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-04-15 20:31:10 -0400

    added dependencies

--- src/components/layout/ProjectLayout.tsx
+++ src/components/layout/ProjectLayout.tsx
@@ -3,7 +3,6 @@ import { Content } from "./project-layout/Content";
 import Footer from "./project-layout/Footer";
 import { Header } from "./project-layout/Header";
 import { Wrapper } from "./project-layout/Wrapper";
-
 /**
  * Responsive web UI layout for RheumInfo.
  * Includes a header with responsive navigation menu and a footer.
