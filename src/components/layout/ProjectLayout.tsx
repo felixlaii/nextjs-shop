2734b7e6072cdda15abcbcb6607ed5b923c9b29f
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-04-15 20:40:02 -0400

    created link array

--- src/components/layout/ProjectLayout.tsx
+++ src/components/layout/ProjectLayout.tsx
@@ -3,11 +3,23 @@ import { Content } from "./project-layout/Content";
 import Footer from "./project-layout/Footer";
 import { Header } from "./project-layout/Header";
 import { Wrapper } from "./project-layout/Wrapper";
+import { useRouter } from "next/router";
+import { NavigationLink } from "@/types/component-types";
+
 /**
  * Responsive web UI layout for RheumInfo.
  * Includes a header with responsive navigation menu and a footer.
  */
 export const ProjectLayout: React.FC<PropsWithChildren> = ({ children }) => {
+  const location = useRouter();
+
+  const navigationLinks: Array<NavigationLink> = [
+    { name: "HOME", href: "/" },
+    { name: "ABOUT", href: "/photo-gallery" },
+    { name: "EXPERTISE", href: "/flavours-pricing" },
+    { name: "EXPERIENCE", href: "/contact-me" },
+  ];
+
   return (
     <Wrapper>
       <Header />
