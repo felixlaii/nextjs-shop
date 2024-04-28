c669342cf491b0ef3e0b4b494d69145bf1deed05
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-04-27 20:41:18 -0400

    changed logo

--- src/components/ui/icons/Logo.tsx
+++ src/components/ui/icons/Logo.tsx
@@ -1,6 +1,6 @@
 import react from "react";
 import Image from "next/image";
-import MacaronLogo from "../../../../public/images/macaron-logo.svg";
+import MacaronLogo from "../../../../public/images/macaron-logo-double.svg";
 
 const Logo = () => {
   return (
