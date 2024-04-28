8342d1f143678d0b9e601d21fd25c1192ed98130
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-04-27 20:18:57 -0400

    added logo

--- src/components/ui/icons/Logo.tsx
+++ src/components/ui/icons/Logo.tsx
@@ -1,2 +1,13 @@
-import { useEffect, useState } from "react";
+import react from "react";
 import Image from "next/image";
+import MacaronLogo from "../../../../public/images/macaron-logo.svg";
+
+const Logo = () => {
+  return (
+    <div>
+      <Image src={MacaronLogo} width={100} height={100} alt="header logo" />
+    </div>
+  );
+};
+
+export default Logo;
