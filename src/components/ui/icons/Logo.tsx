74e5ed44360dcf3c5925334ce5fe3a9f56eeaa62
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-04-30 17:43:03 -0400

    updated logo width

--- src/components/ui/icons/Logo.tsx
+++ src/components/ui/icons/Logo.tsx
@@ -4,7 +4,7 @@ import MacaronLogo from "../../../../public/images/macaron-logo.png";
 const Logo = () => {
   return (
     <div>
-      <Image src={MacaronLogo} width={300} height={300} alt="header logo" />
+      <Image src={MacaronLogo} width={250} height={250} alt="header logo" />
     </div>
   );
 };
