b058bb5abd32cb4976edfa32d00f948693ec22db
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-05-17 22:33:11 -0400

    added header

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -3,12 +3,11 @@ import React from "react";
 const Contact: React.FC = () => {
   return (
     <section>
-      <form
-        name="contact"
-        action="/success"
-        method="POST"
-        data-netlify="true"
-      ></form>
+      <form name="contact" action="/success" method="POST" data-netlify="true">
+        <div>
+          <h2>Lets Talk ...</h2>
+        </div>
+      </form>
     </section>
   );
 };
