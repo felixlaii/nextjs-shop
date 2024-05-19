295a7e7d9bd2ef6783b8d02a0e098c0749171add
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-05-19 16:24:22 -0400

    added flex

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -2,41 +2,69 @@ import React from "react";
 
 const Contact: React.FC = () => {
   return (
-    <section className="flex flex-column">
-      <form name="contact" action="/success" method="POST" data-netlify="true">
-        <div>
-          <h2>Lets Talk ...</h2>
-        </div>
-        <input type="hidden" name="form-name" value="contact" />
-        <label>Name:</label>
-        <input type="text" name="name" placeholder="John/Jane Doe" required />
-        <label>Email:</label>
-        <input
-          placeholder="john/jane@email.com"
-          type="email"
-          name="email"
-          required
-        />
-        <label>Phone:</label>
-        <input placeholder="phone number" type="tel" name="phone" required />
-        <label>Requested Pick Up Date & Time:</label>
-        <input
-          placeholder="date & time"
-          type="datetime-local"
-          name="pickup"
-          required
-        />
-        <label>Message:</label>
-        <textarea
-          placeholder="please include as many details as possible (theme/occasion, quantity, flavours and colours etc)"
-          name="message"
-          required
-        ></textarea>
-        <label>File Attachment:</label>
-        <input type="file" name="file" id="file" required />
+    <section className="flex justify-center min-h-screen p-6">
+      {" "}
+      <div>
+        <form
+          name="contact"
+          action="/success"
+          method="POST"
+          data-netlify="true"
+          className="flex flex-col"
+        >
+          <input type="hidden" name="form-name" value="contact" />
+          <div className="flex-row">
+            <label>Name:</label>
+            <input
+              type="text"
+              name="name"
+              placeholder="John/Jane Doe"
+              required
+            />
+          </div>
+          <div className="flex-row">
+            <label className="">Email:</label>
+            <input
+              placeholder="john/jane@email.com"
+              type="email"
+              name="email"
+              required
+            />
+          </div>
+          <div className="flex-row">
+            <label>Phone:</label>
+            <input
+              placeholder="phone number"
+              type="tel"
+              name="phone"
+              required
+            />
+          </div>
+          <label>Requested Pick Up Date & Time:</label>
+          <div className="flex-row">
+            <input
+              placeholder="date & time"
+              type="datetime-local"
+              name="pickup"
+              required
+            />
+          </div>
+          <div className="flex-row">
+            <label>Message:</label>
+            <textarea
+              placeholder="please include as many details as possible (theme/occasion, quantity, flavours and colours etc)"
+              name="message"
+              required
+            ></textarea>
+          </div>
+          <div className="flex-row">
+            <label>File Attachment:</label>
+            <input type="file" name="file" id="file" required />
+          </div>
 
-        <button type="submit">Submit</button>
-      </form>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </section>
   );
 };
