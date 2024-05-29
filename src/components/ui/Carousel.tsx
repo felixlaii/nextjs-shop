b9fd479a3a01d166533aaee1cb97fbc4fb774f72
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-05-28 21:59:02 -0400

    created state and ref

--- src/components/ui/Carousel.tsx
+++ src/components/ui/Carousel.tsx
@@ -1,8 +1,11 @@
 import Image from "next/image";
-import React, { useEffect, useRef, useState } from "react";
+import React, { use, useEffect, useRef, useState } from "react";
 import { SliderData } from "../../../public/data/carousel-images";
 
 const Carousel = () => {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [carouselSize, setCarouselSize] = useState({ width: 0, height: 0 });
+  const carouselRef = useRef(null);
   return <div></div>;
 };
 
