670f12ad293d0326c4761398466ccb5fb50e5ec9
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-05-28 21:57:29 -0400

    created carousel

--- src/components/ui/Carousel.tsx
+++ src/components/ui/Carousel.tsx
@@ -1,2 +1,9 @@
 import Image from "next/image";
 import React, { useEffect, useRef, useState } from "react";
+import { SliderData } from "../../../public/data/carousel-images";
+
+const Carousel = () => {
+  return <div></div>;
+};
+
+export default Carousel;
