eb2836a6d6b529dca929fd18b6a22864d26cfa00
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-05-28 21:50:23 -0400

    replaced hero image

--- src/components/ui/Hero.tsx
+++ src/components/ui/Hero.tsx
@@ -2,8 +2,14 @@ import Image from "next/image";
 
 const Hero: React.FC = () => {
   return (
-    <div>
-      <Image width={90} height={90} alt="Hero" src="/images/macaron-hero.png" />
+    <div className="flex justify-center max-h-full mt-10 w-full bg-gray-70">
+      <Image
+        width={500}
+        height={500}
+        alt="Hero"
+        src="/images/macaron-boxes-heros.png"
+        className="opacity-40"
+      />
     </div>
   );
 };
