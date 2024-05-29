19250327bb118851a80c8885d9176bae057756fd
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-05-28 22:00:22 -0400

    created useEffect

--- src/components/ui/Carousel.tsx
+++ src/components/ui/Carousel.tsx
@@ -6,6 +6,14 @@ const Carousel = () => {
   const [currentImage, setCurrentImage] = useState(0);
   const [carouselSize, setCarouselSize] = useState({ width: 0, height: 0 });
   const carouselRef = useRef(null);
+
+  useEffect(() => {
+    let elem = carouselRef.current as unknown as HTMLDivElement;
+    let { width, height } = elem.getBoundingClientRect();
+    if (carouselRef.current) {
+      setCarouselSize({ width, height });
+    }
+  }, []);
   return <div></div>;
 };
 
