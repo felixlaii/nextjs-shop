64870fa7bbda10541e02ea5463eca5ab28aebb05
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-05-30 20:28:01 -0400

    added LogoFooter

--- src/components/layout/project-layout/Footer.tsx
+++ src/components/layout/project-layout/Footer.tsx
@@ -1,6 +1,22 @@
 import Image from "next/image";
 import { AiOutlineMail, AiOutlinePhone } from "react-icons/ai";
 
+const LogoFooter = () => {
+  return (
+    <div>
+      <div className="relative">
+        <Image
+          className="w-[8.3rem] h-[2.7rem] md:w-[8rem] md:h-[2.5rem] lg:w-[11.5rem] lg:h-[2.5rem] pt-4 pb-2"
+          width={100}
+          height={40}
+          src="/images/macaron-logo.png"
+          alt="logo"
+        />
+      </div>
+    </div>
+  );
+};
+
 const Footer: React.FC = () => {
   return <div className="w-screen"></div>;
 };
