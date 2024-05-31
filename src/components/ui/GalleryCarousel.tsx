72fd20df87247f0d7c336f6528aa9136b514a299
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-05-30 21:43:34 -0400

    centered carousel

--- src/components/ui/GalleryCarousel.tsx
+++ src/components/ui/GalleryCarousel.tsx
@@ -5,86 +5,92 @@ import { PhotoGallery } from "../../../public/data/photo-gallery";
 
 const GalleryCarousel = () => {
   return (
-    <div id="default-carousel" className="relative w-3/4" data-carousel="slide">
-      <div className="relative h-76 overflow-hidden rounded-lg md:h-96">
-        {PhotoGallery.map((photo, index) => (
-          <div
-            key={index}
-            className={`duration-700 ease-in-out ${
-              index === 0 ? "block" : "hidden"
-            }`}
-            data-carousel-item
-          >
-            <Image
-              src={photo.photo}
-              alt={`Slide ${index + 1}`}
-              width={300} // Set appropriate width
-              height={300} // Set appropriate height
-              className="block w-full h-full object-cover"
-            />
-          </div>
-        ))}
-      </div>
-      <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
-        {PhotoGallery.map((_, index) => (
-          <button
-            key={index}
-            type="button"
-            className="w-3 h-3 rounded-full"
-            aria-current={index === 0 ? "true" : "false"}
-            aria-label={`Slide ${index + 1}`}
-            data-carousel-slide-to={index}
-          ></button>
-        ))}
-      </div>
-      <button
-        type="button"
-        className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-        data-carousel-prev
+    <div className="flex justify-center">
+      <div
+        id="default-carousel"
+        className="relative w-3/4"
+        data-carousel="slide"
       >
-        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-          <svg
-            className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 6 10"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M5 1 1 5l4 4"
-            />
-          </svg>
-          <span className="sr-only">Previous</span>
-        </span>
-      </button>
-      <button
-        type="button"
-        className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-        data-carousel-next
-      >
-        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-          <svg
-            className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 6 10"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="m1 9 4-4-4-4"
-            />
-          </svg>
-          <span className="sr-only">Next</span>
-        </span>
-      </button>
+        <div className="relative h-76 overflow-hidden rounded-lg md:h-96">
+          {PhotoGallery.map((photo, index) => (
+            <div
+              key={index}
+              className={`duration-700 ease-in-out ${
+                index === 0 ? "block" : "hidden"
+              }`}
+              data-carousel-item
+            >
+              <Image
+                src={photo.photo}
+                alt={`Slide ${index + 1}`}
+                width={300} // Set appropriate width
+                height={300} // Set appropriate height
+                className="block w-full h-full object-cover"
+              />
+            </div>
+          ))}
+        </div>
+        <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
+          {PhotoGallery.map((_, index) => (
+            <button
+              key={index}
+              type="button"
+              className="w-3 h-3 rounded-full"
+              aria-current={index === 0 ? "true" : "false"}
+              aria-label={`Slide ${index + 1}`}
+              data-carousel-slide-to={index}
+            ></button>
+          ))}
+        </div>
+        <button
+          type="button"
+          className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+          data-carousel-prev
+        >
+          <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+            <svg
+              className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 6 10"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M5 1 1 5l4 4"
+              />
+            </svg>
+            <span className="sr-only">Previous</span>
+          </span>
+        </button>
+        <button
+          type="button"
+          className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+          data-carousel-next
+        >
+          <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+            <svg
+              className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 6 10"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="m1 9 4-4-4-4"
+              />
+            </svg>
+            <span className="sr-only">Next</span>
+          </span>
+        </button>
+      </div>
     </div>
   );
 };
