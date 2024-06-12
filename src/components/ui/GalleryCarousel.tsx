336e2f03a25b80171b8124c37935a5c6961f01a0
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-06-11 21:55:52 -0400

    added dependencies

--- src/components/ui/GalleryCarousel.tsx
+++ src/components/ui/GalleryCarousel.tsx
@@ -1,94 +1,46 @@
-import React, { useState } from "react";
+import React, { useCallback } from "react";
+import useEmblaCarousel from "embla-carousel-react";
+import Autoplay from "embla-carousel-autoplay";
 import Image from "next/image";
-import "flowbite";
-import { PhotoGallery } from "../../../public/data/photo-gallery";
-
+import PhotoGallery from "@/pages/photogallery";
 const GalleryCarousel = () => {
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true });
+
+  const scrollPrev = useCallback(() => {
+    if (emblaApi) emblaApi.scrollPrev();
+  }, [emblaApi]);
+
+  const scrollNext = useCallback(() => {
+    if (emblaApi) emblaApi.scrollNext();
+  }, [emblaApi]);
   return (
-    <div className="flex justify-center h-screen mt-10">
-      <div
-        id="default-carousel"
-        className="relative w-3/4"
-        data-carousel="slide"
-      >
-        <div className="relative h-2/3 overflow-hidden rounded-lg">
-          {PhotoGallery.map((photo, index) => (
-            <div
-              key={index}
-              className={`duration-700 ease-in-out ${
-                index === 0 ? "block" : "hidden"
-              }`}
-              data-carousel-item
-            >
-              <Image
-                src={photo.photo}
-                alt={`Slide ${index + 1}`}
-                width={800}
-                height={800}
-                className="block w-full h-full object-cover opacity-60"
-              />
-            </div>
-          ))}
+    <div className="embla mx-auto mt-12 max-w-lg">
+      <div className="embla__viewport h-56 border" ref={emblaRef}>
+        <div className="embla__container h-full">
+          <div className="embla__slide flex items-center justify-center">
+            Slide 1
+          </div>
+          <div className="embla__slide flex items-center justify-center">
+            Slide 2
+          </div>
+          <div className="embla__slide flex items-center justify-center">
+            Slide 3
+          </div>
         </div>
-        <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
-          {PhotoGallery.map((_, index) => (
-            <button
-              key={index}
-              type="button"
-              className="w-3 h-3 rounded-full"
-              aria-current={index === 0 ? "true" : "false"}
-              aria-label={`Slide ${index + 1}`}
-              data-carousel-slide-to={index}
-            ></button>
-          ))}
-        </div>
+      </div>
+
+      <div className="mt-3 flex justify-between">
         <button
-          type="button"
-          className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-          data-carousel-prev
+          className="w-20 bg-black px-2 py-1 text-sm text-white"
+          onClick={scrollPrev}
         >
-          <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg
-              className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 6 10"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M5 1 1 5l4 4"
-              />
-            </svg>
-            <span className="sr-only">Previous</span>
-          </span>
+          Prev
         </button>
         <button
-          type="button"
-          className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-          data-carousel-next
+          className="w-20 bg-black px-2 py-1 text-sm text-white"
+          onClick={scrollNext}
         >
-          <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg
-              className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 6 10"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="m1 9 4-4-4-4"
-              />
-            </svg>
-            <span className="sr-only">Next</span>
-          </span>
+          Next
         </button>
       </div>
     </div>
