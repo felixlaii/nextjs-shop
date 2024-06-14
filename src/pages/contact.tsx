79ef0448d164e2e94a649df1d6be458dc567db48
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-06-13 20:16:22 -0400

    Update contact.tsx

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -53,43 +53,56 @@ const Contact: React.FC = () => {
               Email address
             </label>
           </div>
-          <div className="flex flex-col mb-4">
-            <label>Phone</label>
-            <input
-              className="border-b"
-              placeholder="phone number"
-              type="tel"
-              name="phone"
-              required
-            />
-          </div>
-          <div className="flex flex-col mb-4">
-            <label>Date & Time</label>
-            <input
-              id="pickup"
-              className="p-2 border border-gray-300 rounded"
-              type="text"
-              name="pickup"
-              placeholder="date & time"
-              required
-            />
-          </div>
-          <div className="flex flex-col mb-4">
-            <label>Message</label>
-            <textarea
-              className="border px-2"
-              placeholder="please include as many details as possible (theme/occasion, quantity, flavours and colours etc)"
-              name="message"
-              required
-            ></textarea>
-          </div>
-          {/* <div className="flex mb-4">
+          <div className="grid md:grid-cols-2 md:gap-6">
+            <div className="relative z-0 w-full mb-5 group">
+              <input
+                type="tel"
+                pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                name="floating_phone"
+                id="floating_phone"
+                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" "
+                required
+              />
+              <label
+                htmlFor="floating_phone"
+                className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                Phone number (123-456-7890)
+              </label>
+            </div>
+            <div className="relative z-0 w-full mb-5 group">
+              <input
+                id="floating_date"
+                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                type="text"
+                name="floating_date"
+                placeholder=" "
+                required
+              />
+              <label
+                htmlFor="floating_date"
+                className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                date & time
+              </label>
+            </div>
+            <div className="flex flex-col mb-4">
+              <label>Message</label>
+              <textarea
+                className="border px-2"
+                placeholder="please include as many details as possible (theme/occasion, quantity, flavours and colours etc)"
+                name="message"
+                required
+              ></textarea>
+            </div>
+            {/* <div className="flex mb-4">
             <div className="flex flex-col mb-4">
               <label>Inspo</label>
               <input type="file" name="file" id="file" />
             </div>
           </div> */}
-
+          </div>
           <button type="submit">Submit</button>
         </form>
       </div>
