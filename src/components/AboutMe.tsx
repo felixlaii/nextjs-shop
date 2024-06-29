dea0009bddc8aa3a2b7349dfed4e7b3ccc438cf6
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-06-29 17:30:45 -0400

    added description

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -5,6 +5,10 @@ const AboutMe: React.FC = () => {
     <section>
       <div>
         <h3>About Me</h3>
+        <p>
+          Hello, I am Felix <br />I started baking as a hobby, and as a way to
+          show my love to my family and friends.
+        </p>
       </div>
     </section>
   );
