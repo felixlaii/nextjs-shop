5e0bd8354c96f8820121d30d6a8d99b88056491d
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-06-29 17:57:31 -0400

    added FormInquiry interface

--- src/types/form-types.ts
+++ src/types/form-types.ts
@@ -1,3 +1,12 @@
 export interface FormState {
   [key: string]: string;
 }
+
+export interface FormInquiry {
+  floating_name: string;
+  floating_email: string;
+  floating_phone: number;
+  floating_date: string;
+  floating_qty: number;
+  floating_message: string;
+}
