a442d863db770eb4ad201ec5f5fbb7cfbbd88ac3
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-06-29 17:27:26 -0400

    imported About Me into index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Hero from "@/components/ui/Hero";
+import AboutMe from "@/components/AboutMe";
 const Home: React.FC = () => {
   return (
     <div>
       <Hero />
+      <AboutMe />
     </div>
   );
 };
