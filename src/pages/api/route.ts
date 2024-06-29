0035c398b7b5afa2bee193e671dcfa139c09ba31
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-06-29 17:49:21 -0400

    added async function

--- src/pages/api/route.ts
+++ src/pages/api/route.ts
@@ -1,2 +1,11 @@
 import { NextResponse } from "next/server";
 import nodemailer from "nodemailer";
+
+
+export async function POST(req: any) {
+try {
+    const {
+        floating_name, floating_email, floating_phone, floating_date, floating_qty, floating_message
+    } = await req.json
+}
+
