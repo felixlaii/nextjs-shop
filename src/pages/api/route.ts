0b247d052767ed0721c9b73221045dff0a3d0a5b
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-06-29 17:50:00 -0400

    Update route.ts

--- src/pages/api/route.ts
+++ src/pages/api/route.ts
@@ -6,6 +6,6 @@ export async function POST(req: any) {
 try {
     const {
         floating_name, floating_email, floating_phone, floating_date, floating_qty, floating_message
-    } = await req.json
+    } = await req.json()
 }
 
