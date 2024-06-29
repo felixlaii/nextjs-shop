2d222126b5fc0dc6d0eaec1939e46067848ad628
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-06-29 17:53:37 -0400

    added catch

--- src/pages/api/route.ts
+++ src/pages/api/route.ts
@@ -1,33 +1,48 @@
 import { NextResponse } from "next/server";
 import nodemailer from "nodemailer";
 
-
 export async function POST(req: any) {
-try {
+  try {
     const {
-        floating_name, floating_email, floating_phone, floating_date, floating_qty, floating_message
-    } = await req.json()
+      floating_name,
+      floating_email,
+      floating_phone,
+      floating_date,
+      floating_qty,
+      floating_message,
+    } = await req.json();
 
     const transporter = nodemailer.createTransport({
-        service: 'hotmail',
-        host: 'smtpro.zoho.in',
-        port: 587,
-        secure: true,
-        auth: {
-            user: process.env.HOTMAIL_EMAIL,
-            pass: process.env.HOTMAIL_PASS
-        }
-    })
+      service: "hotmail",
+      host: "smtpro.zoho.in",
+      port: 587,
+      secure: true,
+      auth: {
+        user: process.env.HOTMAIL_EMAIL,
+        pass: process.env.HOTMAIL_PASS,
+      },
+    });
 
     const mailOption = {
-        from: 'felix.lai@hotmail.com',
-        to: '',
-        subject: "Macaron Inquiry",
-        html: `
+      from: "felix.lai@hotmail.com",
+      to: "",
+      subject: "Macaron Inquiry",
+      html: `
  
     <li> title: ${floating_name} has an inquiry!</li>
     <li> message: ${floating_message}</li> 
-    `
-    }
-}
+    `,
+    };
+    await transporter.sendMail(mailOption);
 
+    return NextResponse.json(
+      { message: "Email Sent Successfully" },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Failed to Send Email" },
+      { status: 500 }
+    );
+  }
+}
