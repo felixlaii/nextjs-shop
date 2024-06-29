ff8f4deed8f402ed92f1ae9e3ad2c28e9f99df9e
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-06-29 17:51:48 -0400

    created transporter

--- src/pages/api/route.ts
+++ src/pages/api/route.ts
@@ -7,5 +7,16 @@ try {
     const {
         floating_name, floating_email, floating_phone, floating_date, floating_qty, floating_message
     } = await req.json()
+
+    const transporter = nodemailer.createTransport({
+        service: 'hotmail',
+        host: 'smtpro.zoho.in',
+        port: 587,
+        secure: true,
+        auth: {
+            user: process.env.HOTMAIL_EMAIL,
+            pass: process.env.HOTMAIL_PASS
+        }
+    })
 }
 
